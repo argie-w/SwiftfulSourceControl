22259bc4bd410f37b2aec134c0b835f29da3950c
argie-w <reilly.wilkinson@me.com>
2025-05-21 09:25:04 +0200

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by Reilly on 21/05/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
