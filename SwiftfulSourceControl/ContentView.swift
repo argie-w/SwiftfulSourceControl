9c5847defcb7fcc7cee1a382d38412f314236152
argie-w <reilly.wilkinson@me.com>
2025-05-21 10:51:53 +0200

    [Feature] Rename subscribe button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -16,7 +16,7 @@ struct ContentView: View {
             Text("Hello there how are you?!")
         }
         
-        Button("Subscribe Now!") {
+        Button("Subscribe Later!") {
             
         }
         Button("Tap me") {
