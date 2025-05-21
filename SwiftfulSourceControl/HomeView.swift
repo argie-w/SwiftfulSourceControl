1cc27ed2c1751d2a94394bb80c8f7e13f64adfca
argie-w <reilly.wilkinson@me.com>
2025-05-21 10:06:35 +0200

    HomeView finished

--- SwiftfulSourceControl/HomeView.swift
+++ SwiftfulSourceControl/HomeView.swift
@@ -11,6 +11,11 @@ struct HomeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
         Text("SwiftfulSourceControl")
+        Text("Screen 1")
+        VStack {
+            Text("Screen 2")
+            Text("Screen 3")
+        }
     }
 }
 
