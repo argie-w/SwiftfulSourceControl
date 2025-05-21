3527e1e584c3f2692c2bb0eeeea76c8379800b11
argie-w <reilly.wilkinson@me.com>
2025-05-21 16:44:16 +0200

    [Release] 1.0.1 (3)

---
+++ SwiftfulSourceControl/SettingsView.swift
@@ -0,0 +1,19 @@
+//
+//  SettingsView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Reilly on 21/05/25.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    @State private var isSettingsPresented: Bool = false
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SettingsView()
+}
