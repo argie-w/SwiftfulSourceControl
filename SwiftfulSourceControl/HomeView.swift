0a0ebdc553436f6f1942e8e7ac8d8b5556d703ef
argie-w <reilly.wilkinson@me.com>
2025-05-21 10:05:01 +0200

    Add HomeView

---
+++ SwiftfulSourceControl/HomeView.swift
@@ -0,0 +1,19 @@
+//
+//  HomeView.swift
+//  SwiftfulSourceControl
+//
+//  Created by Reilly on 21/05/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("SwiftfulSourceControl")
+    }
+}
+
+#Preview {
+    HomeView()
+}
